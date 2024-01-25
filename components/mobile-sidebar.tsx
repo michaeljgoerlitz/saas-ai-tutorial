fa315dad917fa9a8743ff32f00d5b050c5619a1e
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-24 18:58:05 -0800

    fixed hydration error by adding isMounted to mobile sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,14 +5,26 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    if (!isMounted) {
+        // to prevent hydration error
+        return null;
+    }
+
     return (
         <Sheet>
             <SheetTrigger>
-                {/* <Button variant='ghost' size='icon' className='md:hidden'>
+                <Button variant='ghost' size='icon' className='md:hidden'>
                     <Menu />
-                </Button> */}
+                </Button>
                 <Menu />
             </SheetTrigger>
             <SheetContent side='left' className="p-0">
