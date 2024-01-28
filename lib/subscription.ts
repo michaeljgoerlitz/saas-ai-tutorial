af8890232646b25caf1ad927dfa3a8a835d14330
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-27 21:01:07 -0800

    music generation with replicate ai - it works

---
+++ lib/subscription.ts
@@ -0,0 +1,22 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+const DAY_IN_MS = 86_400_000;
+
+export const checkSubscription = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return false;
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+        where: {
+            userId: userId,
+        },
+        select: {
+            stripeSub
+        }
+    })
+}
