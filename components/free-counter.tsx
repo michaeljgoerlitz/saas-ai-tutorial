140f422a163030b7c61cb3babdf271654e4a7fb1
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-31 21:44:01 -0800

    added progress bar and upgrade button

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -3,6 +3,9 @@
 import { MAX_FREE_COUNTS } from "@/constants";
 import { useEffect, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
 
 interface FreeCounterProps {
     apiLimitCount: number;
@@ -27,8 +30,13 @@ export const FreeCounter = ({
                 <CardContent className="py-6">
                     <div className="text-center text-sm text-white mb-4 space-y-2">
                         <p>
-                            {apiLimitCount} / {MAX_FREE_COUNTS}
+                            {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
                         </p>
+                        <Progress className="h-3" value={(apiLimitCount / MAX_FREE_COUNTS) * 100}/>
+                        <Button className="w-full" variant="premium">
+                            Upgrade
+                            <Zap className="w-4 h-4 ml-2 fill-white"/>
+                        </Button>
                     </div>
                 </CardContent>
             </Card>
