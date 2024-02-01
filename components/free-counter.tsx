5b9b3d2d19da395ceba5267e9398b51b69c0f6e1
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-31 23:03:03 -0800

    almost done with pro modal

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -6,6 +6,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
 import { Zap } from "lucide-react";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FreeCounterProps {
     apiLimitCount: number;
@@ -14,6 +15,7 @@ interface FreeCounterProps {
 export const FreeCounter = ({
     apiLimitCount = 0
 }: FreeCounterProps) => {
+    const proModal = useProModal();
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
@@ -33,7 +35,7 @@ export const FreeCounter = ({
                             {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
                         </p>
                         <Progress className="h-3" value={(apiLimitCount / MAX_FREE_COUNTS) * 100}/>
-                        <Button className="w-full" variant="premium">
+                        <Button onClick={proModal.onOpen} className="w-full" variant="premium">
                             Upgrade
                             <Zap className="w-4 h-4 ml-2 fill-white"/>
                         </Button>
