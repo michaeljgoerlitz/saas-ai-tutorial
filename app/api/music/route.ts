4ce853ed52fd793f767ecfacac6d25298947a0de
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-31 16:40:16 -0800

    added free trial check to conversation, code, music, and video pages - note not image

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -3,6 +3,8 @@ import Replicate from "replicate";
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
+import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
+
 const replicate = new Replicate({
     auth: process.env.REPLICATE_API_TOKEN!
 });
@@ -23,14 +25,22 @@ export async function POST(
             return new NextResponse("Prompt is required", { status: 400 });
         }
 
+        const freeTrial = await checkApiLimit();
+
+        if (!freeTrial) {
+            return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+        }
+
         const response = await replicate.run(
             "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
             {
-              input: {
-                prompt_a: prompt
-              }
+                input: {
+                    prompt_a: prompt
+                }
             }
-          );
+        );
+
+        await increaseApiLimit();
 
         return NextResponse.json(response);
     } catch (error) {
